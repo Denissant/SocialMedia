f22c51444969049536cbe098289231db4c0a1e72
Denissant <denissanturyan@gmail.com>
2021-04-29 23:16:41 +0400

    heroku configuration

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from app.commands import manager
 from app.routes import list_pages, auth, success_register, logoff
 from app.admin.admin_model import AdminModelView, ModModelView
+from app import create_app
 
 if __name__ == '__main__':
     manager.run()
